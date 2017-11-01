86fcf0baa919e1425c1410f181c5e7ef0992f7aa
JoelKan <joelkansala@kamk.fi>
2017-11-01 13:10:37 +0200

    First commit

---
+++ GraphicsTest/TestApplication.cpp
@@ -0,0 +1,53 @@
+/// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+/// "THE BEER-WARE LICENSE" (Revision 42):
+/// <mikko.romppainen@kamk.fi> wrote this file.  As long as you retain this notice you
+/// can do whatever you want with this stuff. If we meet some day, and you think
+/// this stuff is worth it, you can buy me a beer in return. Mikko Romppainen.
+/// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#include <TestApplication.h>
+
+#include <graphics/GraphicsSystem.h>
+#include <graphics/Window.h>
+#include <math.h>
+
+namespace engine
+{
+
+	TestApplication::TestApplication(Window* window, GraphicsSystem* graphics)
+		: GraphicsApplication(window, graphics)
+		, m_totalTime(0.0f)
+	{
+	}
+
+
+	TestApplication::~TestApplication()
+	{
+	}
+
+	void TestApplication::init()
+	{
+
+	}
+
+
+	bool TestApplication::update(float deltaTime)
+	{
+		m_totalTime += deltaTime;
+		return true;
+	}
+
+
+	void TestApplication::render(Window* window, GraphicsSystem* graphics)
+	{
+		(void)window;	
+		float val = fabsf(sinf(2.0f*m_totalTime));
+		
+		// Clear screen with pulsating yellow
+		graphics->clearScreen(val, val * 1.5f, val * 2.0f, true);
+
+
+		// Swap buffers
+		graphics->swapBuffers();
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# graphicsengine-course-project
